64e9e2fdc960fdbdd742b7e27fb64547b01959e7
Satej <Satej>
2023-01-01 07:45:20 +0530

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Satej Sahu on 01/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
